8c9fb4df5f5b3f8ba2cf575ec2a56cd518556562
Seung Hoon Lee <free_minkya@naver.com>
2026-02-02 10:59:28 +0900

    REF-001 : 공통 VO REFEntityTimeMetaData 생성, 앞으로 모든 엔티티에 재사용 예정

---
+++ core_server/src/main/groovy/com/refridge/core_server/config/REFEntityTimeMetaData.java
@@ -0,0 +1,22 @@
+package com.refridge.core_server.config;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Embeddable
+public class REFEntityTimeMetaData {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
