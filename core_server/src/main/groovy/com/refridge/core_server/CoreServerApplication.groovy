215b5151b9e21589c7998668b9f7cfd251c54c6d
Seung Hoon Lee <free_minkya@naver.com>
2026-02-04 02:21:40 +0900

    Revert "REF-001 intermediate merge to main branch : GroceryItem Domain Part Dev"

---
+++ core_server/src/main/groovy/com/refridge/core_server/CoreServerApplication.groovy
@@ -0,0 +1,13 @@
+package com.refridge.core_server
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class CoreServerApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(CoreServerApplication, args)
+    }
+
+}
