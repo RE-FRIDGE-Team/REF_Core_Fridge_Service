3772b445526747f9fa5008d2b334f48dd4c70d71
Seung Hoon Lee <free_minkya@naver.com>
2026-02-05 00:58:40 +0900

    REF-005 : 카테고리명 VO 팩터리 메서드, 검증 메서드 생성

--- core_server/src/main/java/com/refridge/core_server/grocery_category/domain/vo/REFGroceryCategoryName.java
+++ core_server/src/main/java/com/refridge/core_server/grocery_category/domain/vo/REFGroceryCategoryName.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 
+import java.util.Optional;
+
 @Getter
 @Embeddable
 @EqualsAndHashCode
@@ -14,8 +16,19 @@ public class REFGroceryCategoryName {
     @Column(name = "category_name")
     private String value;
 
+    /* CREATION FACTORY METHOD */
     public static REFGroceryCategoryName of(String value) {
-        return new REFGroceryCategoryName(value);
+        return Optional.ofNullable(value)
+                .map(String::trim)
+                .filter(REFGroceryCategoryName::isValidCategoryNameCondition)
+                .map(REFGroceryCategoryName::new)
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "카테고리 이름은 1자 이상 20자 이하여야 합니다."
+                ));
     }
 
+    /* INTERNAL METHOD : 카테고리명 조건 검증 */
+    private static boolean isValidCategoryNameCondition(String categoryName) {
+        return !categoryName.isEmpty() && categoryName.length() <= 20;
+    }
 }
