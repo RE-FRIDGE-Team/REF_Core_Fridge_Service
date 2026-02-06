96f27f8ebea707691bb53334eb1b98392ad3784f
Seung Hoon Lee <free_minkya@naver.com>
2026-02-06 17:32:59 +0900

    REF-007 : GroceryItem 도메인 커스텀 레파지토리 인프라 레이어에 생성

--- core_server/src/main/java/com/refridge/core_server/groceryItem/domain/REFGroceryItemRepository.java
+++ core_server/src/main/java/com/refridge/core_server/groceryItem/domain/REFGroceryItemRepository.java
@@ -4,6 +4,5 @@ import com.refridge.core_server.groceryItem.domain.ar.REFGroceryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface REFGroceryItemRepository extends JpaRepository<REFGroceryItem, Long> {
+public interface REFGroceryItemRepository extends JpaRepository<REFGroceryItem, Long>, REFGroceryItemRepositoryCustom{
 }
---
+++ core_server/src/main/java/com/refridge/core_server/groceryItem/domain/REFGroceryItemRepositoryCustom.java
@@ -0,0 +1,4 @@
+package com.refridge.core_server.groceryItem.domain;
+
+public interface REFGroceryItemRepositoryCustom {
+}
