8c8bcee454d937d050c621ac47577491b68a5911
Seung Hoon Lee <free_minkya@naver.com>
2026-02-06 17:34:29 +0900

    REF-007 : QueryDslConfig 클래스 생성 및 확인

--- core_server/src/main/java/com/refridge/core_server/groceryItem/infra/REFGroceryItemRepositoryImpl.java
+++ core_server/src/main/java/com/refridge/core_server/groceryItem/infra/REFGroceryItemRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.refridge.core_server.groceryItem.infra;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.refridge.core_server.groceryItem.domain.REFGroceryItemRepositoryCustom;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -7,5 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 @RequiredArgsConstructor
 public class REFGroceryItemRepositoryImpl implements REFGroceryItemRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
 
 }
