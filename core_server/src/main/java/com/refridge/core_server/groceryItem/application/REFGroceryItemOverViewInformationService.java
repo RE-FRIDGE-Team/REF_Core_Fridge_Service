07bf33cbba5378414b92ee8c190f7d8c1ca5a4d1
Seung Hoon Lee <free_minkya@naver.com>
2026-02-08 20:12:15 +0900

    REF-007 : @Transactional springframework 패키지로 올바르게 변경

--- core_server/src/main/java/com/refridge/core_server/groceryItem/application/REFGroceryItemOverViewInformationService.java
+++ core_server/src/main/java/com/refridge/core_server/groceryItem/application/REFGroceryItemOverViewInformationService.java
@@ -3,8 +3,10 @@ package com.refridge.core_server.groceryItem.application;
 import com.refridge.core_server.groceryItem.application.dto.REFGroceryItemGetSummaryInfoCommand;
 import com.refridge.core_server.groceryItem.domain.REFGroceryItemRepository;
 import com.refridge.core_server.groceryItem.domain.service.REFGroceryItemCategoryValidatorService;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +16,7 @@ public class REFGroceryItemOverViewInformationService {
 
     private final REFGroceryItemCategoryValidatorService categoryValidatorService;
 
+    @Transactional(readOnly = true)
     public void getGroceryItemSummaryOverViewInformation(REFGroceryItemGetSummaryInfoCommand command) {
 
     }
