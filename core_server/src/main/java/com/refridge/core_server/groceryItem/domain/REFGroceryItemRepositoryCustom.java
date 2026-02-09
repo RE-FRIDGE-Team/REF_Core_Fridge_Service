ab73f7816bc1b029c28678aad4f35c52adc59227
Seung Hoon Lee <free_minkya@naver.com>
2026-02-10 01:46:33 +0900

    REF-007 : GroceryItem 상세 정보 서비스 구현, 매퍼, repository 메서드 생성

--- core_server/src/main/java/com/refridge/core_server/groceryItem/domain/REFGroceryItemRepositoryCustom.java
+++ core_server/src/main/java/com/refridge/core_server/groceryItem/domain/REFGroceryItemRepositoryCustom.java
@@ -3,12 +3,16 @@ package com.refridge.core_server.groceryItem.domain;
 import com.refridge.core_server.groceryItem.infra.persistence.dto.REFGroceryItemDetailDTO;
 import com.refridge.core_server.groceryItem.infra.persistence.dto.REFGroceryItemSummarizedDTO;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface REFGroceryItemRepositoryCustom {
     Optional<REFGroceryItemSummarizedDTO> findSummarizedDTOById(Long id);
+    List<REFGroceryItemSummarizedDTO> findAllSummarizedDTOsByIds(List<Long> ids);
     Optional<REFGroceryItemSummarizedDTO> findSummarizedDTOByItemName(String groceryItemName);
 
     Optional<REFGroceryItemDetailDTO> findDetailDTOById(Long id);
+    List<REFGroceryItemDetailDTO> findAllDetailDTOsByIds(List<Long> ids);
     Optional<REFGroceryItemDetailDTO> findDetailDTOByItemName(String groceryItemName);
+
 }
