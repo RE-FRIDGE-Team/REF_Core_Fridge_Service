b805eb795c695ff1af28765c585f89cf6a0c86ad
Seung Hoon Lee <free_minkya@naver.com>
2026-02-11 14:58:56 +0900

    REF-009 : 이름 기반으로 대분류에서 중분류를 제거하는 서비스 기능 개발

--- core_server/src/main/java/com/refridge/core_server/grocery_category/domain/REFMinorGroceryCategoryRepository.java
+++ core_server/src/main/java/com/refridge/core_server/grocery_category/domain/REFMinorGroceryCategoryRepository.java
@@ -2,8 +2,11 @@ package com.refridge.core_server.grocery_category.domain;
 
 import com.refridge.core_server.grocery_category.domain.ar.REFMajorGroceryCategory;
 import com.refridge.core_server.grocery_category.domain.ar.REFMinorGroceryCategory;
+import com.refridge.core_server.grocery_category.domain.vo.REFGroceryCategoryName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface REFMinorGroceryCategoryRepository extends JpaRepository<REFMinorGroceryCategory, Long>, REFMinorGroceryCategoryRepositoryCustom {
