3e1c64d32196b17d5784e08eecb73efae8768b73
Seung Hoon Lee <free_minkya@naver.com>
2026-02-21 13:09:45 +0900

    REF-015(DOCS) : REFQueryDslConfig 주석 작성

--- core_server/src/main/java/com/refridge/core_server/common/REFQueryDslConfig.java
+++ core_server/src/main/java/com/refridge/core_server/common/REFQueryDslConfig.java
@@ -6,6 +6,22 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/***
+ * {@code REFQueryDslConfig}는 {@code QueryDSL}을 사용하기 위한 설정 클래스입니다.<p>
+ * {@code JPAQueryFactory} 빈을 생성하여, 애플리케이션 전반에서 {@code QueryDSL}울 활용한 쿼리 작성을 가능하게 합니다.<p>
+ * <pre>
+ * {@code
+ * @Repository
+ * @RequiredArgsConstructor
+ * public class REFGroceryItemRepositoryImpl implements REFGroceryItemRepositoryCustom {
+ *
+ *      // 생성자 주입 방식으로 JPAQueryFactory 사용
+ *      private final JPAQueryFactory jpaQueryFactory;
+ *      ....
+ * }
+ * </pre>
+ *
+ */
 @Configuration
 public class REFQueryDslConfig {
 
