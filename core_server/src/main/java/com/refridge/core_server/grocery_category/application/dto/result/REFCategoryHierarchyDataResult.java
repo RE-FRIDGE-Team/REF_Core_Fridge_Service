7a4f3557788384a8b12a677ff9b66e268c932acd
Seung Hoon Lee <free_minkya@naver.com>
2026-03-04 22:11:27 +0900

    REF-015(docs) : REFCategoryHierarchyDataResult 주석

--- core_server/src/main/java/com/refridge/core_server/grocery_category/application/dto/result/REFCategoryHierarchyDataResult.java
+++ core_server/src/main/java/com/refridge/core_server/grocery_category/application/dto/result/REFCategoryHierarchyDataResult.java
@@ -4,6 +4,11 @@ import lombok.Builder;
 
 import java.util.List;
 
+/**
+ * RE:FRIDGE의 식료품 카테고리 계층 구조 메타데이터 조회 결과 DTO입니다.<p>
+ * 대분류 및 중분류에 대한 기본 메타데이터와 함께 분류 속 유니크한 식재료 수를 포함하여 반환합니다.<p>
+ * @param majorCategories 대분류 메타데이터
+ */
 @Builder
 public record REFCategoryHierarchyDataResult (
         List<MajorCategoryWithCount> majorCategories
