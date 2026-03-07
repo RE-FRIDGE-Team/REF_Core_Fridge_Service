fed85496c978f9a60ba2ee07f49f4de6bc154c24
Seung Hoon Lee <free_minkya@naver.com>
2026-03-08 03:27:19 +0900

    REF-017 : REFCsvProductRowDto 주석 수정

--- core_server/src/main/java/com/refridge/core_server/bootstrap/dto/REFCsvProductRowDto.java
+++ core_server/src/main/java/com/refridge/core_server/bootstrap/dto/REFCsvProductRowDto.java
@@ -1,11 +1,23 @@
 package com.refridge.core_server.bootstrap.dto;
 
+import com.refridge.core_server.grocery_category.domain.vo.REFInventoryItemType;
+
+/**
+ * CSV 파일에서 읽어들인 제품 정보를 담는 DTO.<p>
+ * {@link com.refridge.core_server.bootstrap.REFProductAndGroceryItemInitializer}에서 사용한다.
+ * @param originalProductName 원제품명 (CSV 파일에 있는 제품명 그대로)
+ * @param majorCategory 대분류
+ * @param subCategory 중분류
+ * @param inventoryItemType 카테고리 태그 {@link REFInventoryItemType}
+ * @param groceryItemName 식재료명 (GroceryItem 이름으로 사용할 값)
+ * @param brandName 브랜드명
+ */
 public record REFCsvProductRowDto(
-        String originalProductName,   // 원제품명
-        String majorCategory,         // 대분류
-        String subCategory,           // 중분류
-        String inventoryItemType,     // 카테고리태그 (REFInventoryItemType)
-        String groceryItemName,       // 식재료명
-        String brandName              // 브랜드명
+        String originalProductName,
+        String majorCategory,
+        String subCategory,
+        String inventoryItemType,
+        String groceryItemName,
+        String brandName
 ) {
 }
