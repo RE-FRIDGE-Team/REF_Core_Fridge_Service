5490a7c0480ac14c47e1728de47b274f5e1a0988
Seung Hoon Lee <free_minkya@naver.com>
2026-03-08 13:27:38 +0900

    REF-017 : REFGroceryItemDictionaryInitializationStrategy 완성으로, GroceryItem의 이름을 한데 모아 Redis -> Trie 사전 빌드 로직 구현 완료

--- core_server/src/main/java/com/refridge/core_server/groceryItem/domain/REFGroceryItemRepository.java
+++ core_server/src/main/java/com/refridge/core_server/groceryItem/domain/REFGroceryItemRepository.java
@@ -2,7 +2,13 @@ package com.refridge.core_server.groceryItem.domain;
 
 import com.refridge.core_server.groceryItem.domain.ar.REFGroceryItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Set;
 
 
 public interface REFGroceryItemRepository extends JpaRepository<REFGroceryItem, Long>, REFGroceryItemRepositoryCustom{
+
+    @Query("SELECT g.groceryItemName.value FROM REFGroceryItem g")
+    Set<String> findAllGroceryItemNames();
 }
