8749e819cc04cf81f3f0389d01a8afded3b8cb7c
Seung Hoon Lee <free_minkya@naver.com>
2026-03-31 12:30:18 +0900

    [REF-026] REFAsyncConfig 파일 생성

---
+++ core_server/src/main/java/com/refridge/core_server/config/REFAsyncConfig.java
@@ -0,0 +1,9 @@
+package com.refridge.core_server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@Configuration
+public class REFAsyncConfig {
+}
